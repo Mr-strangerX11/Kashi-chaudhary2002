54cdf4c693198fb975031980ffa5de2a38bef936
MacBook <macbook@MacBooks-MacBook-Pro.local>
2025-12-12 21:28:07 +0545

    Fix GitHub Pages routing by using HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import emailjs from '@emailjs/browser'
 import './index.css'
 import App from './App.jsx'
@@ -10,8 +10,8 @@ emailjs.init('KD8PYSkFbR4TgHijc')
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
